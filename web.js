0990e17797be0ed6f0dedf507535e029887fc0e5
pete <pete@odopod.com>
2013-03-14 22:34:43 -0700

    ini

---
+++ web.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var zip = 94110;
+var feedparser = require('feedparser')
+
+
+var app = express.createServer(express.logger());
+app.configure(function(){
+	app.get('/', function(request, response) {
+	  feedparser.parseUrl('http://weather.yahooapis.com/forecastrss?p='+zip).on('article', function(a){
+      response.send(a);
+	  });
+
+	});
+
+});
+
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
